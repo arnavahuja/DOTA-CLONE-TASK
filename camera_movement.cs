941d304ccaaaecec3c803902435ba5836fecdb95
anshu-khare <47294156+anshu-khare@users.noreply.github.com>
2019-06-03 20:40:19 +0530

    Add files via upload

---
+++ camera_movement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class camera_movement : MonoBehaviour {
+    public Transform player;
+     public float RotSpeed = 3.0f , speed = 3.0f;
+    public void FixedUpdate()
+    {     
+        transform.rotation = Quaternion.Slerp(transform.rotation , Quaternion.LookRotation(player.position - transform.position) ,RotSpeed * Time.deltaTime);
+
+    }
+}
