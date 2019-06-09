977fbdfbe2782a40d757121cf5b9c8248b1fac65
Arnav Ahuja <arnavahuja21@gmail.com>
2019-06-09 10:39:15 +0530

    Spawning Added

---
+++ DOTA 2 CLONE UNITY/Assets/Spawner.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] enemies;
+    private int RandNum1,c=0;
+    private int RandNum2,RandNum3,d=0,e=0;
+    public float minwait;
+    public float maxwait;
+    public float startwait1;
+    //public float startwait2;
+    //public float startwait3;
+    public Vector3 spawnvalues;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnwave1());
+        //StartCoroutine(spawnwave2());
+        //StartCoroutine(spawnwave3());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }   
+
+    IEnumerator spawnwave1()
+    {
+        yield return new WaitForSeconds(startwait1);
+        while(c<10)
+        {
+            RandNum1 = Random.Range(0,3);
+            Vector3 spawnplace = new Vector3 (Random.Range(-spawnvalues.x,spawnvalues.x),1,Random.Range(-spawnvalues.z,spawnvalues.z));
+            Instantiate (enemies[RandNum1],spawnplace + transform.TransformPoint(0,0,0), gameObject.transform.rotation);
+            yield return new WaitForSeconds(Random.Range(minwait,maxwait));
+            c++;
+        }
+}
+    /*IEnumerator spawnwave2()
+    {
+        yield return new WaitForSeconds(startwait2);
+        while(d<10)
+        {
+            RandNum2 = Random.Range(0,2);
+            Vector3 spawnplace = new Vector3 (Random.Range(-spawnvalues.x,spawnvalues.x),1,Random.Range(-spawnvalues.z,spawnvalues.z));
+            Instantiate (enemies[RandNum2],spawnplace + transform.TransformPoint(0,0,0), gameObject.transform.rotation);
+            yield return new WaitForSeconds(Random.Range(minwait,maxwait));
+            d++;
+        }
+}
+    IEnumerator spawnwave3()
+    {
+        yield return new WaitForSeconds(startwait3);
+        while(e<10)
+        {
+            RandNum3 = Random.Range(0,3);
+            Vector3 spawnplace = new Vector3 (Random.Range(-spawnvalues.x,spawnvalues.x),1,Random.Range(-spawnvalues.z,spawnvalues.z));
+            Instantiate (enemies[RandNum3],spawnplace + transform.TransformPoint(0,0,0), gameObject.transform.rotation);
+            yield return new WaitForSeconds(Random.Range(minwait,maxwait));
+            e++;
+        }
+}*/
+}
---
+++ DOTA 2 CLONE UNITY/Assets/player_shooting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_shooting : MonoBehaviour
+{
+    public float range=10;
+    public GameObject bullet;
+    public float speed=2;
+        
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+           shoot();
+         }
+    }
+
+
+void shoot ()
+{
+ RaycastHit hit;
+ 
+  if(Physics.Raycast(transform.position,transform.forward,out hit,range))
+    {
+     Debug.Log(hit.transform.name);
+      }
+GameObject newbullet = Instantiate(bullet,transform.position,transform.rotation) as GameObject;
+newbullet.GetComponent<Rigidbody>().velocity = (transform.forward).normalized * speed;
+     
+}
+}
+
+
