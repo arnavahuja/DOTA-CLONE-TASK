1816282825335420da7a700a84c78c2e3b988006
anshu-khare <47294156+anshu-khare@users.noreply.github.com>
2019-06-14 15:21:53 +0530

    Add files via upload
    UPDATED ENEMY DYING

--- enemy_dying.cs
+++ enemy_dying.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class enemy_dying : MonoBehaviour
 {
-    public  int count = 0;
+    public  int count = 2;
      public void healthbar()
      {
-       if(count==2)
+       if(count==0)
        { 
         enemydie();
         }
-        count++;
-        if(count>2)
+        count--;
+        if(count<0)
         {
-        count=0;
+        count=2;
         }
        }
 
