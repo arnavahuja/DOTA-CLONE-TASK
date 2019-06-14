7c9d60438be5367ef0ad0eea15d80d5b57408365
anshu-khare <47294156+anshu-khare@users.noreply.github.com>
2019-06-14 15:22:30 +0530

    Add files via upload
    HEALTHBAR INDICATION

---
+++ healthbar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+    public enemy_dying enemydying;
+    public Slider healthybar;
+     
+    void Start()
+    {
+      healthybar.value=2; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      healthybar.value = enemydying.count;  
+    }
+}
